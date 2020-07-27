ce72e502323e8eebd49649dc2b619b5cf31b05ef
xtrmdarc <diegoreyesco@hotmail.com>
2020-07-27 11:50:41 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode />,
+  document.getElementById('root'),
 );
