2d8d2f4ad3b65a7ef215b9e708e5c486f714e0d4
Julian <artjulius@hotmail.com>
2020-07-30 10:36:10 -0500

    Add CategoryFilter with the select box

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class CategoryFilter extends React.Component {
+  constructor() {
+    super();
+    this.state = { category: "" };
+  }
+
+  render() {
+    const { category } = this.state;
+    const CATEGORIES = [
+      "All",
+      "Action",
+      "Biography",
+      "History",
+      "Horror",
+      "Kids",
+      "Learning",
+      "Sci-Fi",
+    ];
+    return (
+      <label htmlFor="categories">
+        Choose a category
+        <select
+          value={category}
+          name="category"
+          id="categories"
+          onChange={this.handleChange}
+        >
+          {CATEGORIES.map((cat) => (
+            <option value={cat} key={parseInt(Math.random() * 1000, 10)}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </label>
+    );
+  }
+}
+
+export default CategoryFilter;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,48 +1,57 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Book from '../components/Book';
-import { removeBook } from '../actions/index';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Book from "../components/Book";
+import { removeBook } from "../actions/index";
+import CategoryFilter from "../components/CategoryFilter";
 
-const BooksList = props => {
+const BooksList = (props) => {
   const { books, removeBook } = props;
 
-  const handleRemoveBook = book => {
+  const handleRemoveBook = (book) => {
     removeBook(book);
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map(book => (
-          <Book book={book} key={book.id} handleRemoveBook={handleRemoveBook} />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <CategoryFilter />
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <Book
+              book={book}
+              key={book.id}
+              handleRemoveBook={handleRemoveBook}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   books: state.books,
 });
 
-const matchDispatchToProps = dispatch => ({
-  removeBook: book => {
+const matchDispatchToProps = (dispatch) => ({
+  removeBook: (book) => {
     dispatch(removeBook(book));
   },
 });
 
 BooksList.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number, map: PropTypes.func }))
-    .isRequired,
+  books: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number, map: PropTypes.func })
+  ).isRequired,
   removeBook: PropTypes.func.isRequired,
 };
 
