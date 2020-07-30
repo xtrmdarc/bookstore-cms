5466e02b2aee86cebec48de0a715982d3d6cd476
Julian <artjulius@hotmail.com>
2020-07-30 10:16:33 -0500

    Enhance the Redux store, add filter and change filter action with reducer implementation

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import rootReducer from './reducers/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import rootReducer from "./reducers/index";
 
 const store = createStore(rootReducer, {
   books: [
     {
       id: parseInt(Math.random() * 1000, 10),
-      title: 'Harry Potter',
-      category: 'Sci-Fi',
+      title: "Harry Potter",
+      category: "Sci-Fi",
     },
     {
       id: parseInt(Math.random() * 1000, 10),
-      title: 'Holly Bible',
-      category: 'History',
+      title: "Holly Bible",
+      category: "History",
     },
     {
       id: parseInt(Math.random() * 1000, 10),
-      title: 'The hobbit',
-      category: 'Sci-Fi',
+      title: "The hobbit",
+      category: "Sci-Fi",
     },
     {
       id: parseInt(Math.random() * 1000, 10),
-      title: 'Cashvertising',
-      category: 'Learning',
+      title: "Cashvertising",
+      category: "Learning",
     },
   ],
+  filter: "All",
 });
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
